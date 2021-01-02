6f2c623bb6415785ceb371267f8fc3adc79afdfa
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-02 10:23:54 +0000

    Extract regular point scoring to function

--- tennis-refactor/tennis1.js
+++ tennis-refactor/tennis1.js
@@ -1,36 +1,44 @@
 'use strict';
 
 function getScore(player1Score, player2Score) {
-    var score = "";
-    var tempScore = 0;
-
     if (player1Score === player2Score) {
         return scoreForEqualPoints(player1Score);
     } else if (player1Score >= 4 || player2Score >= 4) {
         return scoreAbovePointsRange(player1Score, player2Score);
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) { tempScore = player1Score; }
-            else {
-                score += "-";
-                tempScore = player2Score;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+        return scoreForPoints(player1Score, player2Score);
+    }
+}
+
+const scoreForPoints = (player1Score, player2Score) => {
+    var score = "";
+    var tempScore = 0;
+    
+    for (var i = 1; i < 3; i++) {
+        if (i === 1) {
+            tempScore = player1Score;
+        }
+        else {
+            score += "-";
+            tempScore = player2Score;
+        }
+
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
         }
     }
+
     return score;
 }
 
