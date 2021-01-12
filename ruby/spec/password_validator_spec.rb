c164b8533f5927b95c112dac2d8bf45e0dadc79f
Greg Hynds <60611327+greghynds@users.noreply.github.com>
2021-01-12 00:11:31 +0000

    Improve test names

--- ruby/spec/password_validator_spec.rb
+++ ruby/spec/password_validator_spec.rb
@@ -1,7 +1,7 @@
 require 'password_validator'
 
 RSpec.describe PasswordValidator do
-    it 'password with less than 7 characters is invalid' do
+    it 'returns an error for passwords with less than 7 characters' do
         password = "a1"
         admin = false
         sut = PasswordValidator.new
@@ -11,7 +11,7 @@ RSpec.describe PasswordValidator do
         expect(result[:valid]).to eq false
         expect(result[:errors]).to include("Minimum 7 characters")
     end
-    it 'password with no letters is invalid' do
+    it 'returns an error for passwords which don\'t include letters' do
         password = "12345678"
         admin = false
         sut = PasswordValidator.new
@@ -21,7 +21,7 @@ RSpec.describe PasswordValidator do
         expect(result[:valid]).to eq false
         expect(result[:errors]).to include("Must include at least one letter")
     end
-    it 'password with no numbers is invalid' do
+    it 'returns an error for passwords which don\'t include numbers' do
         password = "abcdefg"
         admin = false
         sut = PasswordValidator.new
@@ -31,7 +31,7 @@ RSpec.describe PasswordValidator do
         expect(result[:valid]).to eq false
         expect(result[:errors]).to include("Must include at least one number")
     end
-    it 'admin password with less than 10 characters is invalid' do
+    it 'returns an error for admin passwords with less than 10 characters' do
         password = "1!cdefg"
         admin = true
         sut = PasswordValidator.new
@@ -41,7 +41,7 @@ RSpec.describe PasswordValidator do
         expect(result[:valid]).to eq false
         expect(result[:errors]).to include("Minimum 10 characters")
     end
-    it 'admin password with no special characters is invalid' do
+    it 'returns an error for admin passwords which don\'t include special characters' do
         password = "1bcdefghij"
         admin = true
         sut = PasswordValidator.new
@@ -51,7 +51,7 @@ RSpec.describe PasswordValidator do
         expect(result[:valid]).to eq false
         expect(result[:errors]).to include("Must include at least one special character (!@#$%^&*)")
     end
-    it 'password with all requirements is valid' do
+    it 'validates passwords which satisfy all requirements' do
         password = "123ABcd"
         admin = false
         sut = PasswordValidator.new
@@ -60,7 +60,7 @@ RSpec.describe PasswordValidator do
 
         expect(result[:valid]).to eq true
     end
-    it 'empty password returns all invalid reasons' do
+    it 'returns multiple errors for empty passwords' do
         password = ""
         admin = false
         sut = PasswordValidator.new
@@ -74,7 +74,7 @@ RSpec.describe PasswordValidator do
 
         expect(result[:errors]).to match_array(errors)
     end
-    it 'empty admin password returns all invalid reasons' do
+    it 'returns multiple errors for empty admin passwords' do
         password = ""
         admin = true
         sut = PasswordValidator.new
@@ -89,7 +89,7 @@ RSpec.describe PasswordValidator do
 
         expect(result[:errors]).to match_array(errors)
     end
-    it 'password with numbers and only upper case letters is valid' do
+    it 'validates passwords with all upper case letters' do
         password = "123ABCD"
         admin = false
         sut = PasswordValidator.new
@@ -98,7 +98,7 @@ RSpec.describe PasswordValidator do
 
         expect(result[:valid]).to eq true
     end
-    it 'password with numbers and only upper case letters is valid' do
+    it 'validates password with all lower case letters' do
         password = "123abcd"
         admin = false
         sut = PasswordValidator.new
@@ -107,7 +107,7 @@ RSpec.describe PasswordValidator do
 
         expect(result[:valid]).to eq true
     end
-    it 'admin password with all requirements is valid' do
+    it 'validates admin passwords which satisfy all requirements' do
         password = "!1cdefghij"
         admin = true
         sut = PasswordValidator.new
