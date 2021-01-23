62d68ccac4b4c9254ec311c9cf8b0f80a17f5d35
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-23 23:56:42 +0000

    Move password validator kata into separate folder

--- ruby/lib/includes_letters.rb
+++
@@ -1,8 +0,0 @@
-require 'password_condition'
-
-class IncludesLetters < PasswordCondition
-    def check(password, admin)
-        valid = !(password =~ /[a-zA-Z]/).nil?
-        valid ? nil : "Must include at least one letter"
-    end
-end
--- ruby/lib/includes_numbers.rb
+++
@@ -1,8 +0,0 @@
-require 'password_condition'
-
-class IncludesNumbers < PasswordCondition
-    def check(password, admin)
-        valid = !(password =~ /[0-9]/).nil?
-        valid ? nil : "Must include at least one number"
-    end
-end
