c75404e5596e7c68fbe8b67389062173b062faf0
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-23 23:03:14 +0000

    Remove instance variables from factory

--- ruby/lib/parrot_factory.rb
+++ ruby/lib/parrot_factory.rb
@@ -5,20 +5,17 @@ require 'african_parrot'
 class ParrotFactory
 
     def initialize type, number_of_coconuts, voltage, nailed
-      @type = type
-      @number_of_coconuts = number_of_coconuts
-      @voltage = voltage
-      @nailed = nailed
+      @parrot = case type
+      when :european_parrot
+        EuropeanParrot.new
+      when :african_parrot
+        AfricanParrot.new(number_of_coconuts)
+      when :norwegian_blue_parrot
+        NorwegianParrot.new(voltage, nailed)
+      end
     end
   
     def speed
-      case @type
-      when :european_parrot
-        EuropeanParrot.new.speed
-      when :african_parrot
-        AfricanParrot.new(@number_of_coconuts).speed
-      when :norwegian_blue_parrot
-        NorwegianParrot.new(@voltage, @nailed).speed
-      end
+      @parrot.speed
     end
 end
