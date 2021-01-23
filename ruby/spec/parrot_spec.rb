8499ec7d364ba16fe7b4cb3be6b55d89455530d6
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-23 23:34:15 +0000

    Rename test methods

--- ruby/spec/parrot_spec.rb
+++ ruby/spec/parrot_spec.rb
@@ -1,37 +1,37 @@
 require 'parrot_factory'
 
-describe "Parrots" do
-  it "gets speed of a European parrot" do
+describe "ParrotFactory" do
+  it "calculates speed of a European parrot" do
     parrot = ParrotFactory.new(:european_parrot, 0, 0, false)
     expect(parrot.speed).to eq(12.0)
   end
 
-  it "gets speed of an African parrot with one coconut" do
+  it "calculates speed of an African parrot with one coconut" do
     parrot = ParrotFactory.new(:african_parrot, 1, 0, false)
     expect(parrot.speed).to eq(3.0)
   end
 
-  it "gets speed of an African parrot with two coconuts" do
+  it "calculates speed of an African parrot with two coconuts" do
     parrot = ParrotFactory.new(:african_parrot, 2, 0, false)
     expect(parrot.speed).to eq(0.0)
   end
 
-  it "gets speed of an African parrot with no coconuts" do
+  it "calculates speed of an African parrot with no coconuts" do
     parrot = ParrotFactory.new(:african_parrot, 0, 0, false)
     expect(parrot.speed).to eq(12.0)
   end
 
-  it "gets speed of a nailed Norwegian blue parrot" do
+  it "calculates speed of a nailed Norwegian blue parrot" do
     parrot = ParrotFactory.new(:norwegian_blue_parrot, 0, 1.5, true)
     expect(parrot.speed).to eq(0.0)
   end
 
-  it "gets speed of a not nailed Norwegian blue parrot" do
+  it "calculates speed of a not nailed Norwegian blue parrot" do
     parrot = ParrotFactory.new(:norwegian_blue_parrot, 0, 1.5, false)
     expect(parrot.speed).to eq(18.0)
   end
 
-  it "gets speed of a not nailed Norwegian blue parrot with high voltage" do
+  it "calculates speed of a not nailed Norwegian blue parrot with high voltage" do
     parrot = ParrotFactory.new(:norwegian_blue_parrot, 0, 4, false)
     expect(parrot.speed).to eq(24.0)
   end
