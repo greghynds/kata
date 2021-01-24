f02685ffe73f81453c8105e766d972b89c618c5c
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-24 22:12:36 +0000

    Refactor tests

--- ruby/racing_car/spec/ticket_dispenser_spec.rb
+++ ruby/racing_car/spec/ticket_dispenser_spec.rb
@@ -1,23 +1,32 @@
 require 'ticket_dispenser'
 
 describe TicketDispenser do
-  it "dispenses a ticket with the next turn number" do
-    expected = Ticket.new(0).number
+  it "dispenses a ticket with the next number" do
+    expected = 0
     sut = TicketDispenser.new(TicketSequence.new)
     
     actual = sut.dispense.number
 
     expect(actual).to eq(expected)
   end
-  it "dispenses next ticket with a higher turn number" do
-    expectedFirst = Ticket.new(0).number
-    expectedSecond = Ticket.new(1).number
+  it "dispenses a second ticket with a higher number than the first" do
     sut = TicketDispenser.new(TicketSequence.new)
     
     first = sut.dispense.number
     second = sut.dispense.number
 
-    expect(first).to eq(expectedFirst)
-    expect(second).to eq(expectedSecond)
+    expect(first).to eq(0)
+    expect(second).to eq(1)
+  end
+  it "keeps in sync with other dispensers" do
+    sequence = TicketSequence.new
+    dispenser1 = TicketDispenser.new(sequence)
+    dispenser2 = TicketDispenser.new(sequence)
+    
+    first = dispenser1.dispense.number
+    second = dispenser2.dispense.number
+
+    expect(first).to eq(0)
+    expect(second).to eq(1)
   end
 end
