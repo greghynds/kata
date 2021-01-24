d565a52d91cb373e2d13d563944a6e319911fca3
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-24 15:17:30 +0000

    Refactor tests to AAA

--- ruby/parrots/spec/parrot_spec.rb
+++ ruby/parrots/spec/parrot_spec.rb
@@ -2,42 +2,74 @@ require 'parrot_factory'
 
 describe "ParrotFactory" do
   it "calculates speed of a European parrot" do
-    parrot = ParrotFactory.new(:european_parrot)
-    expect(parrot.speed).to eq(12.0)
+    sut = ParrotFactory.new(:european_parrot)
+    expected = 12.0
+    
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of an African parrot with one coconut" do
-    parrot = ParrotFactory.new(:african_parrot, number_of_coconuts: 1)
-    expect(parrot.speed).to eq(3.0)
+    sut = ParrotFactory.new(:african_parrot, number_of_coconuts: 1)
+    expected = 3.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of an African parrot with two coconuts" do
-    parrot = ParrotFactory.new(:african_parrot, number_of_coconuts: 2)
-    expect(parrot.speed).to eq(0.0)
+    sut = ParrotFactory.new(:african_parrot, number_of_coconuts: 2)
+    expected = 0.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of an African parrot with no coconuts" do
-    parrot = ParrotFactory.new(:african_parrot, number_of_coconuts: 0)
-    expect(parrot.speed).to eq(12.0)
+    sut = ParrotFactory.new(:african_parrot, number_of_coconuts: 0)
+    expected = 12.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of a nailed Norwegian blue parrot" do
-    parrot = ParrotFactory.new(:norwegian_blue_parrot, voltage: 1.5, nailed: true)
-    expect(parrot.speed).to eq(0.0)
+    sut = ParrotFactory.new(:norwegian_blue_parrot, voltage: 1.5, nailed: true)
+    expected = 0.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of a not nailed Norwegian blue parrot" do
-    parrot = ParrotFactory.new(:norwegian_blue_parrot, voltage: 1.5)
-    expect(parrot.speed).to eq(18.0)
+    sut = ParrotFactory.new(:norwegian_blue_parrot, voltage: 1.5)
+    expected = 18.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "calculates speed of a not nailed Norwegian blue parrot with high voltage" do
-    parrot = ParrotFactory.new(:norwegian_blue_parrot, voltage: 4)
-    expect(parrot.speed).to eq(24.0)
+    sut = ParrotFactory.new(:norwegian_blue_parrot, voltage: 4)
+    expected = 24.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 
   it "returns speed of zero for unknown types of parrot" do
-    parrot = ParrotFactory.new(:unknown_parrot)
-    expect(parrot.speed).to eq(0.0)
+    sut = ParrotFactory.new(:unknown_parrot)
+    expected = 0.0
+
+    actual = sut.speed
+
+    expect(actual).to eq(expected)
   end
 end
