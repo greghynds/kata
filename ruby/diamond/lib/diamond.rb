bd75904b29ed986b3171d049c96e711d0635c71e
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-27 04:15:05 +0000

    Refactor Diamon to use constructor instantiation

--- ruby/diamond/lib/diamond.rb
+++ ruby/diamond/lib/diamond.rb
@@ -1,7 +1,9 @@
 require 'diamond_quadrant'
 
 class Diamond
-    def self.for(letter, filler = '.')
+    attr_reader :rows
+
+    def initialize(letter, filler = '.')
         quadrant = DiamondQuadrant.new(letter, filler)
         top_right = quadrant.row(:top_right)
         top_left = quadrant.row(:top_left)
@@ -10,16 +12,18 @@ class Diamond
         top_row = join_horizontal(top_left, top_right)
         bottom_row = join_horizontal(bottom_left, bottom_right)
         
-        join_vertical(top_row, bottom_row)
+        @rows = join_vertical(top_row, bottom_row)
     end
 
-    def self.join_horizontal(left, right)
+    private
+
+    def join_horizontal(left, right)
         left.each_with_index.map do |row, i|
             row[0, row.length - 1] + right[i]
         end
     end
 
-    def self.join_vertical(top, bottom)
+    def join_vertical(top, bottom)
         top.take(top.length - 1) + bottom
     end
 end
