a74397cdff69353b74f5f94e6cd4ba77b8251691
Greg Hynds <greg.hynds@bbc.co.uk>
2021-01-27 02:42:34 +0000

    Add quadrant position variable

--- ruby/diamond/lib/diamond_quadrant.rb
+++ ruby/diamond/lib/diamond_quadrant.rb
@@ -2,7 +2,14 @@ class DiamondQuadrant
     LETTERS = "ABCDEFGHIJKLMNOPQRSTUVXYZ".split('')
     private_constant :LETTERS
 
-    def self.for(letter)
+    def self.for(letter, position = :top_right)
+        case position
+        when :top_right then top_right(letter)
+        else top_right(letter)
+        end
+    end
+
+    def self.top_right(letter)
         total_rows = LETTERS.index(letter) + 1
         total_rows.times.each_with_index.map do |i|
             l = LETTERS[i]
--- ruby/diamond/spec/diamond_quadrant_spec.rb
+++ ruby/diamond/spec/diamond_quadrant_spec.rb
@@ -3,19 +3,19 @@ require 'diamond_quadrant'
 describe DiamondQuadrant do
   it "returns top right quadrant for A" do
     expected = ['A']
-    actual = DiamondQuadrant.for('A')
+    actual = DiamondQuadrant.for('A', :top_right)
 
     expect(actual).to eq(expected)
   end
   it "returns top right quadrant for B" do
     expected = ['A.', '.B']
-    actual = DiamondQuadrant.for('B')
+    actual = DiamondQuadrant.for('B', :top_right)
 
     expect(actual).to eq(expected)
   end
   it "returns top right quadrant for C" do
     expected = ['A..', '.B.', '..C']
-    actual = DiamondQuadrant.for('C')
+    actual = DiamondQuadrant.for('C', :top_right)
 
     expect(actual).to eq(expected)
   end
