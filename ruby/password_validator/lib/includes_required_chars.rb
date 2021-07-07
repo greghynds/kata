9a06ca234c0b57de0cbd0a8a154539fef70ff453
Greg Hynds <greg.hynds@bbc.co.uk>
2021-07-07 23:40:50 +0100

    Add checkType() in IncludesRequiredChars

--- ruby/password_validator/lib/includes_required_chars.rb
+++ ruby/password_validator/lib/includes_required_chars.rb
@@ -2,7 +2,14 @@ require 'password_condition'
 
 class IncludesRequiredChars < PasswordCondition
     def check(password, admin)
-        valid = !admin || !(password =~ /[!@#$%^&*]/).nil?
-        valid ? nil : "Must include at least one special character (!@#$%^&*)"
+        checkType(password, UserType.from(admin))
+    end
+
+    def checkType(password, userType)
+        valid = case userType
+        when :admin_user; !(password =~ /[!@#$%^&*]/).nil?
+        else; true
+        end
+        return valid ? nil : "Must include at least one special character (!@#$%^&*)"
     end
 end
